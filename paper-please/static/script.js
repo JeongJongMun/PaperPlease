d28722ba6dc5b85cdd2c0c56363835162923c348
정종문 <whdans4005@naver.com>
2023-12-02 18:48:47 +0900

    1202 1848 JJM

--- paper-please/static/script.js
+++ paper-please/static/script.js
@@ -15,8 +15,10 @@ async function question() {
         success: function (response) {
             if (response['result'] == 'success') {
                 console.log("Query Success", response['answer'])
-                $('#queryOutput').append(`${response['answer']}`)
-                $('#queryOutput').append(`<br>`)     
+                let tempHtml = `
+                <p class="w-full p-2 border border-secondary border-2 rounded-2">${response['answer']}</p>
+                `;
+                $('#queryOutput').append(tempHtml)
             } else {
                 alert('쿼리 실패')
             }
