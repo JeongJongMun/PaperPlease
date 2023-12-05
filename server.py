5d4098247c21b1250007659c704f44ba60a5a295
정종문 <whdans4005@naver.com>
2023-12-06 08:13:06 +0900

    1206 0813 JJM

--- server.py
+++ server.py
@@ -5,9 +5,8 @@ from fastapi.templating import Jinja2Templates
 from fastapi import HTTPException
 from fastapi.staticfiles import StaticFiles
 from langserve import add_routes, RemoteRunnable
-
-from langchain_utils import agent_executor, pp
-from libraries import *
+from langchain.schema.runnable import RunnableLambda
+from langchain_utils import update_config, agent_executor, pp
 
 app = FastAPI(
   title="Paper Please",
@@ -21,7 +20,11 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 # Agent Input Must be a dict
-make_dict = lambda x: {'input': x}
+# make_dict = lambda x: {'input': x}
+
+def tt(x):
+    print(type(x))
+    return x
 
 @app.get("/")
 def read_root(request: Request):
@@ -31,22 +34,27 @@ def read_root(request: Request):
         return {"error": str(e)}
     except Exception as e:
         return {"error": str(e)}
-
+    
 add_routes(
     app,
-    RunnableLambda(make_dict) | RunnableLambda(pp) | agent_executor | RunnableLambda(pp),
+    RunnableLambda(tt) | RunnableLambda(pp) 
+    | RunnableLambda(update_config)
+    | agent_executor
+    | RunnableLambda(pp),
     path="/chat",
     
 )
 
+
 """Sample Questions
-Who is the Author of "Attention is All You Need"?
-Which AI Paper is Hottest in 2023? Describe it in 3 sentences.
-Tell me about Attention is All You Need. Describe it in 3 sentences.
-Who is the Author of 'GLUE: A Multi-Task Benchmark and Analysis Platform for Natural Language Understanding'?
+"Attention is All You Need"의 저자는 누구인가요?
+2023년에 가장 인기 있는 AI 논문은 무엇인가요?
+Attention is All You Need에 대해서 설명해줘.
+BERT: Pre-training of Deep Bidirectional Transformers for Language Understanding의 주된 주제에 대해서 설명해줘.
+'GLUE: A Multi-Task Benchmark and Analysis Platform for Natural Language Understanding'의 저자는 누구인가요?
 """
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="localhost", port=8001)
+    uvicorn.run(app, host="localhost", port=8000)
